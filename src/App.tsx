61491579783ee81ce67fb934dbeaea842709758b
noylevi <noylevi91@gmail.com>
2023-10-22 20:28:18 +0200

    install mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import {
+  Routes,
+  Route,
+  BrowserRouter,
+  useLocation,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+import LoginPage from "./components/login-page/LoginPage";
+import Home from "./components/home/Home";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="login-page">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginPage />}></Route>
+            <Route element={<PrivateRoutes />}>
+              <Route path="/home" element={<Home />}></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
+
+function checkToken(): boolean {
+  return true;
+}
+
+const PrivateRoutes = () => {
+  const { pathname } = useLocation();
+
+  const [isValidToken, setIsValidToken] = useState<boolean>(); // todo use context instead
+
+  useEffect(() => {
+    // initial mount or route changed, check token
+    setIsValidToken(checkToken());
+  }, [pathname]);
+
+  if (isValidToken === undefined) {
+    return null; // or loading indicator/spinner/etc
+  }
+
+  return isValidToken ? <Outlet /> : <Navigate to="/" replace />;
+};
 
 export default App;
---
+++ src/components/home/Home.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Home() {
+  return <div className="login-page">this is home page</div>;
+}
