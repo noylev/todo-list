bc698f78f57d6d1ed93da4d105fdfccb14fbd6ce
noylevi <noylevi91@gmail.com>
2023-10-24 10:41:29 +0200

    wrap the App with the context of the user and changed the validity check of the authToken to fit to the mock BE call

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./App.css";
 import {
   Routes,
@@ -10,37 +10,48 @@ import {
 } from "react-router-dom";
 import LoginPage from "./components/login-page/LoginPage";
 import Home from "./components/home/Home";
+import { AuthContext } from "./context/AuthContext";
+import { useLocalStorage } from "./hooks/useLocalStorage";
+import { checkIsValidToken } from "./api";
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />}></Route>
-          <Route element={<PrivateRoutes />}>
-            <Route path="/home" element={<Home />}></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-}
+  const { user, setUser } = useContext(AuthContext);
 
-function checkToken(): boolean {
-  return true;
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginPage />}></Route>
+            <Route element={<PrivateRoutes />}>
+              <Route path="/home" element={<Home />}></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </AuthContext.Provider>
+  );
 }
 
 const PrivateRoutes = () => {
   const { pathname } = useLocation();
+  const { getItem } = useLocalStorage();
 
-  const [isValidToken, setIsValidToken] = useState<boolean>(); // todo use context instead
+  const [isValidToken, setIsValidToken] = useState<boolean>(true);
 
   useEffect(() => {
     // initial mount or route changed, check token
-    setIsValidToken(checkToken());
+    const authToken: string = getItem("authToken");
+    checkIsValidToken(authToken)
+      .then((isValid) => {
+        setIsValidToken(isValid);
+      })
+      .catch(() => {
+        setIsValidToken(false);
+      });
   }, [pathname]);
 
   if (isValidToken === undefined) {
-    return null; // or loading indicator/spinner/etc
+    return null;
   }
 
   return isValidToken ? <Outlet /> : <Navigate to="/" replace />;
