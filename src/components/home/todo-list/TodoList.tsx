c9823a8ddbb962a27f42729de56453afb9a50bf7
noylevi <noylevi91@gmail.com>
2023-10-25 12:47:04 +0200

    add support to edit task, bug fixed, added better ux

--- src/components/home/todo-list/TodoList.tsx
+++ src/components/home/todo-list/TodoList.tsx
@@ -1,45 +1,64 @@
 import React, { useState } from "react";
 import "./TodoList.css";
 import { Button, List, TextField } from "@mui/material";
-import TaskItem, { Task } from "./TaskItem";
+import TaskItem, { Task } from "./task-item/TaskItem";
 
-const emptyTask = { value: "", isDone: false };
 export default function TodoList() {
   const [todoList, setTodoList] = useState<Task[]>([]);
-  const [newTask, setNewTask] = useState<Task>(emptyTask);
+  const [value, setValue] = useState<string>("");
+  function getTaskInitialValues(): Task {
+    return {
+      key: todoList.length + 1,
+      value: value,
+      isDone: false,
+    };
+  }
 
   function addTask(): void {
-    setTodoList([...todoList, newTask as Task]);
-    setNewTask(emptyTask);
+    const newTask = getTaskInitialValues();
+    setTodoList([...todoList, newTask]);
+    setValue("");
   }
-  function removeTask(value: string): void {
-    const newList = todoList.filter((task) => task.value !== value);
+  function removeTask(taskToRemove: Task): void {
+    const newList = todoList.filter((task) => task.key !== taskToRemove.key);
     setTodoList(newList);
   }
+
+  function editTask(editedTask: Task): void {
+    const newList = todoList.map((task) => {
+      if (task.key === editedTask.key) {
+        task.value = editedTask.value;
+      }
+      return task;
+    });
+
+    setTodoList([...newList]);
+  }
+
   return (
     <div className="list">
       <div className="add-task">
         <TextField
           label="Add your new task here"
           variant="standard"
-          value={newTask.value}
+          fullWidth
+          value={value}
           onKeyDown={(event) => {
             if (event.key === "Enter") {
               addTask();
             }
           }}
-          onChange={(event) =>
-            setNewTask({ ...newTask, value: event.target?.value })
-          }
+          onChange={(event) => setValue(event.target?.value)}
         />
-        <Button onClick={addTask} disabled={!newTask}>
+        <Button onClick={addTask} disabled={!value}>
           Add
         </Button>
       </div>
-
       <List>
         {todoList.map((task, key) => {
-          return <TaskItem key={key} task={task} onDelete={removeTask} />;
+          return (
+            <TaskItem task={task} onDelete={removeTask} onChange={editTask} />
+          );
         })}
       </List>
     </div>
---
+++ src/components/home/todo-list/task-item/TaskItem.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import { useBoolean } from "ahooks";
+import { Checkbox, ListItem, Button, TextField } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import "./TaskItem.css";
+export interface Task {
+  key: number;
+  value: string;
+  isDone: boolean;
+}
+export interface TaskItemProps {
+  task: Task;
+  onChange: (task: Task) => void;
+  onDelete?: (task: Task) => void;
+}
+export default function TaskItem({ task, onChange, onDelete }: TaskItemProps) {
+  const [isMarkedDone, setIsMarkedDone] = useBoolean(task.isDone);
+  const [isEditMode, setIsEditMode] = useBoolean(false);
+  const [value, setValue] = useState<string>(task.value);
+
+  // This effect is for when value of the task gets changed and component should be re-rendered
+  useEffect(() => {
+    setValue(task.value);
+  }, [task.value]);
+
+  return (
+    <ListItem
+      key={task.key}
+      disablePadding
+      secondaryAction={
+        <div className="actions">
+          <Button
+            disabled={isMarkedDone}
+            onClick={() => {
+              setIsEditMode.setTrue();
+            }}
+          >
+            edit
+          </Button>
+          <div className="delete-icon" onClick={() => onDelete?.(task)}>
+            <DeleteIcon />
+          </div>
+        </div>
+      }
+    >
+      <Checkbox
+        edge="start"
+        checked={isMarkedDone}
+        onChange={() => setIsMarkedDone.toggle()}
+      />
+      <TextField
+        value={value}
+        className={isMarkedDone ? "mark-task-done" : "mark-task-active"}
+        aria-readonly={true}
+        variant="standard"
+        onChange={(event) => {
+          setValue(event.target.value);
+        }}
+        onBlur={() => {
+          setIsEditMode.setFalse();
+          onChange({ ...task, value });
+        }}
+        style={{ width: 400 }}
+        InputProps={{
+          readOnly: !isEditMode,
+          disableUnderline: true,
+        }}
+      />
+    </ListItem>
+  );
+}
