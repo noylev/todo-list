813f17298b5a27d94ccc6ed49129e3569679093a
noylevi <noylevi91@gmail.com>
2023-10-28 15:04:51 +0200

    tests

--- src/components/home/todo-list/TodoList.test.tsx
+++ src/components/home/todo-list/TodoList.test.tsx
@@ -2,24 +2,25 @@ import React from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
 import TodoList from "./TodoList";
 
-test("should render that the page is rendered correctly", () => {
+function setup() {
   render(<TodoList />);
-  const newTaskInput = screen.getByText("Add your new task here");
-  const addButton = screen.getByRole("button", {
-    name: /Add/i,
-  });
-  expect(newTaskInput).toBeInTheDocument();
-  expect(addButton).toBeInTheDocument();
-});
-
-test("should add new todo task item to the list once user click `add`", () => {
-  render(<TodoList />);
-  const newTaskInput: HTMLInputElement = screen.getByLabelText(
+  const newTaskInput: HTMLInputElement = screen.getByText(
     "Add your new task here",
   );
   const addButton = screen.getByRole("button", {
     name: /Add/i,
   });
+
+  return { newTaskInput, addButton };
+}
+test("should render that the page is rendered correctly", () => {
+  const { newTaskInput, addButton } = setup();
+  expect(newTaskInput).toBeInTheDocument();
+  expect(addButton).toBeInTheDocument();
+});
+
+test("should add new todo task item to the list once user click `add`", () => {
+  const { newTaskInput, addButton } = setup();
   fireEvent.change(newTaskInput, { target: { value: "Go to buy groceries" } });
   expect(newTaskInput.value).toContain("Go to buy groceries");
 
