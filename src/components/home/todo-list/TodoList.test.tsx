07cbfb6bbe30c1851cd385ab46308f4d8713cf09
noylevi <noylevi91@gmail.com>
2023-10-28 15:01:57 +0200

    more tests

--- src/components/home/todo-list/TodoList.test.tsx
+++ src/components/home/todo-list/TodoList.test.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import TodoList from "./TodoList";
 
-test("renders learn react link", () => {
+test("should render that the page is rendered correctly", () => {
   render(<TodoList />);
   const newTaskInput = screen.getByText("Add your new task here");
   const addButton = screen.getByRole("button", {
@@ -10,4 +10,20 @@ test("renders learn react link", () => {
   });
   expect(newTaskInput).toBeInTheDocument();
   expect(addButton).toBeInTheDocument();
+});
+
+test("should add new todo task item to the list once user click `add`", () => {
+  render(<TodoList />);
+  const newTaskInput: HTMLInputElement = screen.getByLabelText(
+    "Add your new task here",
+  );
+  const addButton = screen.getByRole("button", {
+    name: /Add/i,
+  });
+  fireEvent.change(newTaskInput, { target: { value: "Go to buy groceries" } });
+  expect(newTaskInput.value).toContain("Go to buy groceries");
+
+  fireEvent.click(addButton);
+
+  expect(screen.getByRole("listitem")).toHaveTextContent("edit");
 });
